63651f449bf358ec655e7526ab669d5447a49b01
Gino Murin <gmurin@gmail.com>
2023-08-10 22:50:58 -0400

    Working on location selection portion of bookingflow in Location.Picker.jsx. Just started building the component. Need to bring in styles fom AppointmentPicker.jsx, filling in the data from locations.jsx including an image.

---
+++ locations.js
@@ -0,0 +1,25 @@
+export const locations = [
+    {
+        location_id:1,
+        housecall: false,
+        name: "ABC Photography",
+        address_1: "555 Grant St",
+        address_2: "Suite 337",
+        city: "Pittsburgh",
+        state: "PA",
+        zip:"15219",
+        description:null,
+        img:'./assets/map.png'
+    },
+    {
+        location_id:2,
+        name: "Your Location",
+        address_1: null,
+        address_2: null,
+        city: null,
+        state: null,
+        zip:null,
+        description:"We'll Come To You!",
+        img:'./assets/map.png'
+    }
+]
--- services.js
+++ services.js
@@ -1,5 +1,6 @@
 export const services = [
     {
+        eid:1,
         name:"Classic Headshot - In Studio",
         description:"Efficient session to get an amazing photo. Up to 15 minutes max. Immediate photo proofing. One finished image, retouched with licensing, included. Additional images are $85 each.",
         price:175.00,
@@ -7,6 +8,7 @@ export const services = [
         inOffice:true
     },
     {
+        eid:2,
         name:"Legendary Headshot - In Studio",
         description:"A little more time to perfect your look; quick clothing change if you like. Up to 30 minutes max. Immediate photo proofing. Two finished images, retouched with licensing, included. Additional images are $85 each",
         price:335.00,
@@ -14,6 +16,7 @@ export const services = [
         inOffice:true
     },
     {
+        eid:3,
         name:"Team photo (2-4) & Headshot Photos - In Studio",
         description:"Team photo of up to 4 people, and 4 Headshot Photos. Up to 1.5 hrs in studio. Includes 1 finished team photo and 1 finished image of each person.",
         price:795.00,
@@ -21,6 +24,7 @@ export const services = [
         inOffice:true
     },
     {
+        eid:4,
         name:"Ultimate Headshot - In Studio",
         description:"Time to do whatever you want in studio. No limits for outfit changes/looks. Up to 2 hours max.  Immediate photo proofing. Three finished images, retouched with licensing, included. Additional images are $85 each.",
         price:620.00,
@@ -28,6 +32,7 @@ export const services = [
         inOffice:true
     },
     {
+        eid:5,
         name:"Passport/Visa Photos - In Studio",
         description:"Images cropped to country's specifications. 2-4 printed photos, depending on size, and digital file.",
         price:79.00,
@@ -35,6 +40,7 @@ export const services = [
         inOffice:true
     },
     {
+        eid:6,
         name:"Passport/Visa Photos - In Studio",
         description:"Images cropped to country's specifications. 2-4 printed photos, depending on size, and digital file.",
         price:79.00,
@@ -42,6 +48,7 @@ export const services = [
         inOffice:true
     },
     {
+        eid:7,
         name:"Passport/Visa Photos - In Studio",
         description:"This is an add-on to our Headshot Sessions. Same-day or next-morning delivery. (Not needed for passport/visa session).",
         price:50.00,
@@ -50,6 +57,7 @@ export const services = [
         addOn:true
     },
     {
+        eid:8,
         name:"Urban/Outdoor Portrait - Downtown",
         description:"(May-Oct) An outdoor session in Mellon Square or surrounding blocks around my studio, with Up-to-2-hours reserved, including time for proofing images in studio. Includes 3 finished photos. (Nov-Apr) Will be more limited as far as where to shoot, lighting, and how long we can spend outside... but we can make it work!",
         price:790.00,
